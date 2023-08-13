a7553128d2ea40c9d14a5b0fd5dfc98f3c2b44eb
Aseem Ratha <aseemratha@gmail.com>
2023-08-13 18:15:22 -0400

    afasf

--- src/app/components/map.tsx
+++ src/app/components/map.tsx
@@ -2,8 +2,12 @@ var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiYWFyYXRoYSIsImEiOiJjbGw5eWZ6anExaDJtM2VtenpuZHJ1c2FuIn0.TOfFT2m_2oDuHFgjEFYiYg';
 var map = new mapboxgl.Map({
-    container: 'YOUR_CONTAINER_ELEMENT_ID',
+    container: 'map-render',
     style: 'mapbox://styles/mapbox/streets-v11'
 });
+
+export default function SimpleMap() {
+    <div id="map-render">Map</div>
+}
 
 
