7886c45e1bf8dcc050b588c5185570bf2f61b427
Aseem Ratha <aseemratha@gmail.com>
2023-08-13 19:42:45 -0400

    fagagad

--- src/app/components/map.tsx
+++ src/app/components/map.tsx
@@ -7,9 +7,10 @@ import mapboxgl from 'mapbox-gl';
 export default function SimpleMap():any {
     let lat=40;
     let lng=30;
-    
+
     const handleClick:any = () => {
         console.log('clicked!');
+        geolocate.trigger();
     }
 
     useEffect(() => {
@@ -17,26 +18,26 @@ export default function SimpleMap():any {
         const map = new mapboxgl.Map({
             container: 'map', // container ID
             // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-            style: 'mapbox://styles/mapbox/streets-v12', // style URL
+            style: 'mapbox://styles/aaratha/clla30j0600uh01p83743h1xa', // style URL
             center: [lng, lat], // starting position [lng, lat]
             zoom: 17 // starting zoom
         });
-        map.addControl(
-            new mapboxgl.GeolocateControl({
+        const geolocate = new mapboxgl.GeolocateControl({
             positionOptions: {
             enableHighAccuracy: true
             },
-            // When active the map will receive updates to the device's location as it changes.
-            trackUserLocation: true,
-            // Draw an arrow next to the location dot to indicate which direction the device is heading.
-            showUserHeading: true
-            })
-            );
+            trackUserLocation: true
+            });
+            // Add the control to the map.
+            map.addControl(geolocate);
+            map.on('load', () => {
+            geolocate.trigger();
+            });
     }, []);
 
     return (
         <div>
-            <div id="map" style={{ width: '50vw', height: '60vh' }}></div>
+            <div id="map" style={{ width: '49vw', height: '80vh' }} className='rounded-full overflow-auto border berder-white border-opacity-25'></div>
             <button onClick={handleClick}>Find</button>
         </div>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center">
       <div>
-        <h1 className='text-4xl mt-20 mb-20'>Pin It!</h1>
+        <h1 className='text-4xl mt-10 mb-10'>Pin It!</h1>
       </div>
       <SimpleMap />
     </main>
