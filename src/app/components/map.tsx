935882fcff1a777d63261376fdf0884915823a7b
Aseem Ratha <aseemratha@gmail.com>
2023-08-14 15:03:07 -0400

    ffahjg

--- src/app/components/map.tsx
+++ src/app/components/map.tsx
@@ -1,16 +1,17 @@
 'use client';
-import React from 'react';
-import { useRef } from 'react';
+import React, { Fragment } from 'react';
 
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
+import { createPortal } from 'react-dom';
 
 export default function SimpleMap():any {
     const geolocateRef = useRef<mapboxgl.GeolocateControl | null>(null);
     const mapRef = useRef<mapboxgl.Map | null>(null);
+    const [markers, setMarkers] = useState<mapboxgl.Marker[]>([]);
 
     useEffect(() => {
-        mapboxgl.accessToken = 'pk.eyJ1IjoiYWFyYXRoYSIsImEiOiJjbGw5eWZ6anExaDJtM2VtenpuZHJ1c2FuIn0.TOfFT2m_2oDuHFgjEFYiYg';
+        mapboxgl.accessToken = 'pk.eyJ1IjoiYWFyYXRoYSIsImEiOiJjbGxiOG9xdDMwNG56M2txbWNhbzl1Nm1iIn0.t7OS87HS14To9irKMOAh6A';
         
         const map = new mapboxgl.Map({
             container: 'map',
@@ -32,26 +33,35 @@ export default function SimpleMap():any {
             geolocateRef.current?.trigger();
         });
         mapRef.current = map;
-    }, []);
+
+        markers.forEach(marker => { marker.addTo(map); });
+    }, [markers]);
 
     const handleClick = () => {
         geolocateRef.current?.trigger();
     };
-
-    mapRef.current?.on('click', (e: mapboxgl.MapMouseEvent) => {
-        console.log(e.lngLat);
-        const marker = new mapboxgl.Marker()
-            .setLngLat(e.lngLat)
-            .addTo(mapRef.current!);
-    });
+    useEffect(() => {
+        mapRef.current?.on('click', (e: mapboxgl.MapMouseEvent) => {
+            console.log(e.lngLat);
+            const marker = new mapboxgl.Marker()
+                .setLngLat(e.lngLat)
+                .addTo(mapRef.current!);
+            // setMarkers([...markers, marker]);
+            setMarkers(prevMarkers => {
+                const newMarkers = [...prevMarkers, marker];
+                console.log(newMarkers);
+                return newMarkers;
+            });
+        });
+    }, [markers]);  
 
     return (
         <div>
-            <div className=' rounded-3xl overflow-hidden border border-black w-[92vh] h-[72vh] flex justify-center'>
-                <div id="map" className='rounded-3xl overflow-hidden border border-black w-[90vh] h-[70vh] m-auto'></div>
-
+            <div className=' rounded-3xl overflow-hidden border border-black w-[92vw] md:w-[92vh] h-[72vh] flex justify-center z-10'>
+                <div id="map" className='rounded-3xl overflow-hidden border border-black w-[90vw]  md:w-[90vh] h-[70vh] m-auto'>
+                </div>
             </div>
-            <button onClick={handleClick} className='border border-black border-opacity-25 p-4 pt-2 pb-2 rounded-full hover:bg-white text-black transition-all absolute -translate-y-20 translate-x-10 bg-gray-500 bg-opacity-25'>Find</button>
+            <button onClick={handleClick} className='border border-black border-opacity-25 p-4 pt-2 pb-2 rounded-full hover:bg-white text-black transition-all absolute -translate-y-20 translate-x-10 bg-gray-500 bg-opacity-25 z-20'>Find</button>
         </div>
     );
 }
