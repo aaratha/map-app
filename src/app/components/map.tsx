b8abcbd1fc26f95fe9879c0521dd6599d74505bd
Aseem Ratha <aseemratha@gmail.com>
2023-08-14 00:56:05 -0400

    fasfad

--- src/app/components/map.tsx
+++ src/app/components/map.tsx
@@ -1,44 +1,50 @@
 'use client';
 import React from 'react';
+import { useRef } from 'react';
 
 import { useEffect } from 'react';
 import mapboxgl from 'mapbox-gl';
 
 export default function SimpleMap():any {
-    let lat=40;
-    let lng=30;
-
-    const handleClick:any = () => {
-        console.log('clicked!');
-        geolocate.trigger();
-    }
+    const geolocateRef = useRef<mapboxgl.GeolocateControl | null>(null);
 
     useEffect(() => {
         mapboxgl.accessToken = 'pk.eyJ1IjoiYWFyYXRoYSIsImEiOiJjbGw5eWZ6anExaDJtM2VtenpuZHJ1c2FuIn0.TOfFT2m_2oDuHFgjEFYiYg';
+        
         const map = new mapboxgl.Map({
-            container: 'map', // container ID
-            // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-            style: 'mapbox://styles/aaratha/clla30j0600uh01p83743h1xa', // style URL
-            center: [lng, lat], // starting position [lng, lat]
-            zoom: 17 // starting zoom
+            container: 'map',
+            style: 'mapbox://styles/aaratha/clla30j0600uh01p83743h1xa',
+            center: [30, 40],
+            zoom: 1
         });
+        const marker = new mapboxgl.Marker()
+            .setLngLat([30, 40])
+            .addTo(map); // add the marker to the map
+
         const geolocate = new mapboxgl.GeolocateControl({
             positionOptions: {
-            enableHighAccuracy: true
+                enableHighAccuracy: true
             },
             trackUserLocation: true
-            });
-            // Add the control to the map.
-            map.addControl(geolocate);
-            map.on('load', () => {
-            geolocate.trigger();
-            });
+        });
+        
+        map.addControl(geolocate);
+        geolocateRef.current = geolocate;
+        map.on('load', () => {
+            geolocateRef.current?.trigger();
+        });
     }, []);
+
+    const handleClick = () => {
+        geolocateRef.current?.trigger();
+    };
 
     return (
         <div>
-            <div id="map" style={{ width: '49vw', height: '80vh' }} className='rounded-full overflow-auto border berder-white border-opacity-25'></div>
-            <button onClick={handleClick}>Find</button>
+            <div className=' rounded-3xl overflow-hidden border border-black w-[92vh] h-[72vh] flex justify-center'>
+                <div id="map" className='rounded-3xl overflow-hidden border border-black w-[90vh] h-[70vh] m-auto'></div>
+            </div>
+            <button onClick={handleClick} className='border border-black border-opacity-25 p-4 pt-2 pb-2 rounded-full hover:bg-white text-black transition-all absolute -translate-y-20 translate-x-10 bg-gray-500 bg-opacity-25'>Find</button>
         </div>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import SimpleMap from './components/map'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center">
+    <main className="flex text-black min-h-screen flex-col items-center bg-gradient-to-b from-gray-200 to-gray-300">
       <div>
         <h1 className='text-4xl mt-10 mb-10'>Pin It!</h1>
       </div>
