e5589a9245f15c1a4f5f273d1afef64bc759f61f
Aseem Ratha <aseemratha@gmail.com>
2023-08-21 22:43:18 -0400

    pwa

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require("@ducanh2912/next-pwa").default({
+    dest: "public",
+});
+
+module.exports = withPWA({
     images: {
         domains: ['lh3.googleusercontent.com'],
         unoptimized: true,
     },
-}
-
-module.exports = nextConfig
+})
